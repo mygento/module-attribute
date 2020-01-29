af768701fbcd5b1808e2dfdd1840186e44a35172
nikita tarasov <nikita@mygento.ru>
2020-01-29 12:32:48 +0300

    init

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2020 Mygento (https://www.mygento.ru)
+ * @package Mygento_Attribute
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mygento_Attribute',
+    __DIR__
+);
